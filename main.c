1eebcb92f6cd1dfe92019be5ba5f3ca2545f8f0d
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-08-23 14:14:25 -0700

    test project

--- main.c
+++ main.c
@@ -1,18 +1,89 @@
 #include "main.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @argv: arg vector
- *
- * Return: 0 on success, 1 on error
+ * sig_handler - checks if Ctrl C is pressed
+ * @sig_num: int
  */
-
-
-int main(int ac, char **argv)
+void sig_handler(int sig_num)
 {
-	char *prompt = "(Myshell) $ ";
-	printf("%s", prompt);
-	return (0);
+	if (sig_num == SIGINT)
+	{
+		_puts("\n#cisfun$ ");
+	}
 }
 
+/**
+* _EOF - handles the End of File
+* @len: return value of getline function
+* @buff: buffer
+ */
+void _EOF(int len, char *buff)
+{
+	(void)buff;
+	if (len == -1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			_puts("\n");
+			free(buff);
+		}
+		exit(0);
+	}
+}
+/**
+  * _isatty - verif if terminal
+  */
+
+void _isatty(void)
+{
+	if (isatty(STDIN_FILENO))
+		_puts("#cisfun$ ");
+}
+/**
+ * main - Shell
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	ssize_t len = 0;
+	char *buff = NULL, *value, *pathname, **arv;
+	size_t size = 0;
+	list_path *head = '\0';
+	void (*f)(char **);
+
+	signal(SIGINT, sig_handler);
+	while (len != EOF)
+	{
+		_isatty();
+		len = getline(&buff, &size, stdin);
+		_EOF(len, buff);
+		arv = splitstring(buff, " \n");
+		if (!arv || !arv[0])
+			execute(arv);
+		else
+		{
+			value = _getenv("PATH");
+			head = lpath(value);
+			pathname = _which(arv[0], head);
+			f = chkbuild(arv);
+			if (f)
+			{
+				free(buff);
+				f(arv);
+			}
+			else if (!pathname)
+				execute(arv);
+			else if (pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute(arv);
+			}
+		}
+	}
+	free_list(head);
+	freearv(arv);
+	free(buff);
+	return (0);
+}
