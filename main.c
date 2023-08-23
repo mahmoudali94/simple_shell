964c873363f24a5e265155546be30a9e9ff6e5d7
mahmoudali94 <eng.mahmoudali94@gmail.com>
2023-08-23 12:30:24 -0700

    create main files

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @ac: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+
+int main(int ac, char **argv){
+
+   return (0);
+}
+
